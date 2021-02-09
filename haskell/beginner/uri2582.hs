e09a9a3a998079084c0190d1d26649b8dd1a2dc5
Murilo <m2l_0@outlook.com>
2021-02-09 14:04:45 -0300

    haskell: solution-2582

---
+++ haskell/beginner/uri2582.hs
@@ -0,0 +1,20 @@
+module Main where
+
+musics :: Int -> [Char]
+musics = (["PROXYCITY","P.Y.N.G.","DNSUEY!","SERVERS","HOST!","CRIPTONIZE","OFFLINE DAY","SALT","ANSWER!","RAR?","WIFI ANTENNAS"] !!)
+
+selMusic :: Int -> Int -> String
+selMusic a b = musics $ (a + b) `mod` 11
+
+ans :: Int -> IO ()
+ans 0 = putStr ""
+ans n = do
+    input <- getLine
+    let [a,b] = map read (words input) :: [Int]
+    putStrLn $ selMusic a b
+    ans (n - 1)
+
+main :: IO ()
+main = do
+    x <- readLn :: IO Int
+    ans x
