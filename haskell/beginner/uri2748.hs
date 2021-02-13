bb8483c9eea63ed244e94f3407f51f0e37a86c07
Murilo <m2l_0@outlook.com>
2021-02-13 15:52:34 -0300

    beginner: 2748

---
+++ haskell/beginner/uri2748.hs
@@ -0,0 +1,10 @@
+module Main where
+
+main = do
+    putStrLn "---------------------------------------"
+    putStrLn "|        Roberto                      |"
+    putStrLn "|                                     |"
+    putStrLn "|        5786                         |"
+    putStrLn "|                                     |"
+    putStrLn "|        UNIFEI                       |"
+    putStrLn "---------------------------------------"
