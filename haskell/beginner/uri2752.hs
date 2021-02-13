dc78554930c6aa5427e200dc9c599112046b02f1
Murilo <m2l_0@outlook.com>
2021-02-13 15:52:56 -0300

    beginner: 2752

---
+++ haskell/beginner/uri2752.hs
@@ -0,0 +1,12 @@
+module Main where
+
+main :: IO ()
+main = do
+    putStrLn "<AMO FAZER EXERCICIO NO URI>"
+    putStrLn "<    AMO FAZER EXERCICIO NO URI>"
+    putStrLn "<AMO FAZER EXERCICIO >"
+    putStrLn "<AMO FAZER EXERCICIO NO URI>"
+    putStrLn "<AMO FAZER EXERCICIO NO URI    >"
+    putStrLn "<AMO FAZER EXERCICIO NO URI>"
+    putStrLn "<          AMO FAZER EXERCICIO >"
+    putStrLn "<AMO FAZER EXERCICIO           >"
